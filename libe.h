817fcb70f741f160669a9b2520ecb66cedfbe984
Kurt Van Dijck <dev.kurt@vandijck-laurijssen.be>
2015-08-19 20:36:53 +0200

    license: Add (L)GPL

--- libe.h
+++ libe.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2015 Kurt Van Dijck <dev.kurt@vandijck-laurijssen.be>
+ *
+ * This file is part of libet.
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef _libe_h_
 #define libe_h_
 #ifdef __cplusplus
